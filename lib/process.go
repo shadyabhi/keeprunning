45b3a2c4c886709b085ca1087c24658131e6a94a
Abhijeet Rastogi <me@abhijeetr.com>
2016-07-02 17:41:55 +0530

    Sleep for a second before retrying

--- lib/process.go
+++ lib/process.go
@@ -16,6 +16,7 @@ func startProc(cmdPath []string, output io.Writer) (*exec.Cmd, error) {
 
 	if err := c.Start(); err != nil {
 		// Backoff and try again in some time
+		time.Sleep(1 * time.Second)
 		return nil, errors.Wrap(err, "Error starting process. Will sleep and retry")
 	}
 	return c, nil
